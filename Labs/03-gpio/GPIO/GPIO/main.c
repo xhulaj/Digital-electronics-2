5e3fad421e0ee46eff9629af22ccfaa94ce433f8
Josef Hula <xhulaj00@vutbr.cz>
2020-10-07 13:13:18 +0200

    update wed

---
+++ Labs/03-gpio/GPIO/GPIO/main.c
@@ -0,0 +1,18 @@
+/*
+ * GPIO.c
+ *
+ * Created: 07.10.2020 13:08:05
+ * Author : spravce
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
