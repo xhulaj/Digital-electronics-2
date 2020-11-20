8f5598e5e9cb92632a509d701d360370e272b60d
Martin Eliáš <xelias19@vutbr.cz>
2020-11-20 15:23:44 +0100

    reformatated structure of files

---
+++ Labs/project/keypad/keypad/keypad/main.c
@@ -0,0 +1,18 @@
+/*
+ * keypad.c
+ *
+ * Created: 20.11.2020 15:14:29
+ * Author : Elias
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
